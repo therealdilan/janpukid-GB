cb689e7518b4f60d35034cb47a28c5059bf6dff9
dilan <125643732+therealdilan@users.noreply.github.com>
2023-11-10 00:18:26 +0400

    working backgrounds, tiles and scroll

--- janpukid.c
+++ janpukid.c
@@ -2,18 +2,26 @@
 #include <stdio.h>
 
 #include "assets/janpu.c"
+#include "assets/world1map.c"
+#include "assets/world1tiles.c"
 
 
 void main() 
 {
+    set_bkg_data(0,10,world1tiles);
+    set_bkg_tiles(0,0,40,18,world1map);
+
     UINT8 currentSpriteIndex = 0;
     set_sprite_data(0, 2, janpu);
     set_sprite_tile(0, 0);
     move_sprite(0, 88, 78);
+    SHOW_BKG;
     SHOW_SPRITES;
+    DISPLAY_ON;
 
     while (1)
     {
+        scroll_bkg(1, 0);
         switch (joypad())
         {
         case J_LEFT:
