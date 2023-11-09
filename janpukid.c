8e967eb04b81399ddb24f24f3dd25d080047fb3b
dilan <125643732+therealdilan@users.noreply.github.com>
2023-11-09 18:11:00 +0400

    basic sprite + movement

--- janpukid.c
+++ janpukid.c
@@ -1,6 +1,46 @@
+#include <gb/gb.h>
 #include <stdio.h>
+
+#include "assets/janpu.c"
+
 
 void main() 
 {
-    printf("Hello, World!");
+    UINT8 currentSpriteIndex = 0;
+    set_sprite_data(0, 2, janpu);
+    set_sprite_tile(0, 0);
+    move_sprite(0, 88, 78);
+    SHOW_SPRITES;
+
+    while (1)
+    {
+        switch (joypad())
+        {
+        case J_LEFT:
+            scroll_sprite(0, -1, 0);
+            currentSpriteIndex = 1;
+            set_sprite_tile(0, currentSpriteIndex);
+            break;
+        case J_RIGHT:
+            currentSpriteIndex = 0;
+            scroll_sprite(0, 1, 0);
+            set_sprite_tile(0, currentSpriteIndex);
+            break;
+        case J_UP:
+            currentSpriteIndex = 0;
+            scroll_sprite(0, 0, -1);
+            set_sprite_tile(0, currentSpriteIndex);
+            break;
+        case J_DOWN:
+            currentSpriteIndex = 1;
+            scroll_sprite(0, 0, 1);
+            set_sprite_tile(0, currentSpriteIndex);
+            break;
+        default:
+            break;
+        }
+        delay(10);
+    }
+    
 }
+
